288409217f7d9a4c95ed8ced5af8841699bea26b
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-09 20:53:47 +0100

    Add files .gitignore and .gitattributes

--- src/main/java/it/unibo/TestAllClasses.java
+++ src/main/java/it/unibo/TestAllClasses.java
@@ -4,10 +4,12 @@ import it.unibo.animal.*;
 
 public class TestAllClasses {
     public static void main(String[] args) {
-        Animal kangaroo = new Kangaroo(50.0);
-        Animal korat = new KoratCat(4.0);
-        Animal siamese = new SiameseCat(7.0);
-        Animal squirrel = new Squirrel(0.60);
+        Animal kangaroo = new Kangaroo(50.0, "Macropodidae", "Macropus");
+        Animal korat = new KoratCat(4.0, "Felidae", "Felis catus");
+        Animal siamese = new SiameseCat(7.0, "Felidae", "Felis catus");
+        Animal squirrel = new Squirrel(0.60, "Sciuridae", "Sciurus");
+
+        Animal[] animals = {kangaroo, korat, siamese, squirrel};
 
         // Test the canEat method
         // everything should print true
@@ -15,5 +17,12 @@ public class TestAllClasses {
         System.out.println(korat.canEat(squirrel));
         System.out.println(!siamese.canEat(kangaroo));
         System.out.println(!squirrel.canEat(korat));
+
+        // Test family and specie methods
+        for (Animal a : animals) {
+            System.out.println(a.getClass().getSimpleName().toUpperCase() + ":");
+            System.out.println("Family: " + a.getFamily());
+            System.out.println("Specie: " + a.getSpecies());
+        }
     }
 }
